468dba538968a0fba8f2054b8d7ccba2331c0ff9
xonoxo143-ux <xonoxo143@gmail.com>
2026-04-26 17:51:03 -0400

    Add bridge section model

---
+++ app/src/main/java/com/applab/termuxbridge/bridge/BridgeSection.kt
@@ -0,0 +1,10 @@
+package com.applab.termuxbridge.bridge
+
+enum class BridgeSection(val title: String) {
+    SETUP("Setup"),
+    REPO("GitHub / Repo"),
+    APK("APK"),
+    SAVE("Save Codes"),
+    AUDIT("Source Audits"),
+    DEBUG("Debug Bundle")
+}
